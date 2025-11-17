534b416e9fc5b609faa6d3fea0cedfac2c026cb8
Suyash-D27 <yashdaharwal27@gmail.com>
2025-11-17 13:05:40 +0530

    all cofiguration done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from "fs"
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_CLOUD_APIKEY, 
+  api_secret: process.env.CLOUDINARY_CLOUD_API_SCERET
+});
+
+const fileUploaderOnCloudinary = async(localFilepath)=>{
+    try {
+        if(!localFilepath) return null;
+        // upload file in cloudinary
+        const respond = await cloudinary.uploader.upload(localFilepath,{
+            resource_type:"auto"
+        });
+        console.log("file is uploaded in the public url is", respond.url)
+        return respond;
+
+    } catch (error) {
+        fs.unlinkSync(localFilepath)// delete the save  temporary file in sever if error have while uploading
+        return null; 
+    }
+} 
+
+export{fileUploaderOnCloudinary}
