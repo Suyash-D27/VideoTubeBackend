fe43cdb62cd3b5cd1ffbae656020c602fce9be5c
Suyash-D27 <yashdaharwal27@gmail.com>
2025-11-18 21:23:02 +0530

    user controller done and some bug fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controller/user.controller.js";
+import { upload } from "../middelware/multer.middelware.js"; 
 
 const userRouter = Router();
 
-userRouter.route("/register").post(registerUser)
+userRouter.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser
+)
 
 
 
